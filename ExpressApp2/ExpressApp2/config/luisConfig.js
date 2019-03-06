086964a0941f490b06e43df9c68283ebf28f4569
junhpark <junhpark@taihoinst.com>
2019-03-06 18:24:59 +0900

    init commit
    init commit

---
+++ ExpressApp2/ExpressApp2/config/luisConfig.js
@@ -0,0 +1,7 @@
+var luisConfig = {
+    //subKey: 'db5c8e83e84f4c9e9c21f5da0b5a48fd',
+    subKey: '7b74c4d4f8314efd8db75589f53c4493',
+    HOST_URL: 'https://westus.api.cognitive.microsoft.com' // Luis api host not available -https://eastasia.api.cognitive.microsoft.com
+};
+
+module.exports = luisConfig;
