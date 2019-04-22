350af77b603d28e8d46edb72bbdd2298a8a0f113
민혁 장 <jmh9898847@naver.com>
2019-04-22 13:44:45 +0900

    Image filtering operation.

--- ExpressApp2/ExpressApp2/public/js/common/upload.js
+++ ExpressApp2/ExpressApp2/public/js/common/upload.js
@@ -41,6 +41,24 @@ function InsertFileUpload() {
     }
 }
 
+// //파일 업로드 시 이미지 확장자 validation
+// function chk_file_type(obj) {
+//     var file_kind = obj.value.lastIndexOf('.');
+//     var file_name = obj.value.substring(file_kind+1,obj.length);
+//     var file_type = file_name.toLowerCase();
+     
+//     var check_file_type=new Array();​
+   
+//     check_file_type=['jpg','gif','png','jpeg','bmp'];
+   
+//     if(check_file_type.indexOf(file_type)==-1){
+//      alert('이미지 파일만 선택할 수 있습니다.');
+//      var parent_Obj=obj.parentNode
+//      var node=parent_Obj.replaceChild(obj.cloneNode(true),obj);
+//      return false;
+//     }
+// }
+
 //파일 리스트 출력
 function makeUploadTable() {
     params = {
